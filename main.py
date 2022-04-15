e40d49777888d2f033420a6cc4cf0bc957f911d2
叶芃 <isdkfj@qq.com>
2022-04-15 22:51:06 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from train import *
 from module import CAWN
 from graph import NeighborFinder
 import resource
+from sklearn.metrics import roc_auc_score
 
 args, sys_argv = get_args()
 
@@ -79,16 +80,34 @@ else:
     valid_test_new_old_flag = (valid_test_flag.astype(int) - valid_test_new_new_flag.astype(int)).astype(bool)
     logger.info('Sampled {} nodes (10 %) which are masked in training and reserved for testing...'.format(len(mask_node_set)))
 
-# split data according to the mask
-train_src_l, train_dst_l, train_ts_l, train_e_idx_l, train_label_l = src_l[valid_train_flag], dst_l[valid_train_flag], ts_l[valid_train_flag], e_idx_l[valid_train_flag], label_l[valid_train_flag]
-val_src_l, val_dst_l, val_ts_l, val_e_idx_l, val_label_l = src_l[valid_val_flag], dst_l[valid_val_flag], ts_l[valid_val_flag], e_idx_l[valid_val_flag], label_l[valid_val_flag]
-test_src_l, test_dst_l, test_ts_l, test_e_idx_l, test_label_l = src_l[valid_test_flag], dst_l[valid_test_flag], ts_l[valid_test_flag], e_idx_l[valid_test_flag], label_l[valid_test_flag]
-if args.mode == 'i':
-    test_src_new_new_l, test_dst_new_new_l, test_ts_new_new_l, test_e_idx_new_new_l, test_label_new_new_l = src_l[valid_test_new_new_flag], dst_l[valid_test_new_new_flag], ts_l[valid_test_new_new_flag], e_idx_l[valid_test_new_new_flag], label_l[valid_test_new_new_flag]
-    test_src_new_old_l, test_dst_new_old_l, test_ts_new_old_l, test_e_idx_new_old_l, test_label_new_old_l = src_l[valid_test_new_old_flag], dst_l[valid_test_new_old_flag], ts_l[valid_test_new_old_flag], e_idx_l[valid_test_new_old_flag], label_l[valid_test_new_old_flag]
-train_data = train_src_l, train_dst_l, train_ts_l, train_e_idx_l, train_label_l
-val_data = val_src_l, val_dst_l, val_ts_l, val_e_idx_l, val_label_l
-train_val_data = (train_data, val_data)
+if args.tune:
+    train_src_l = src_l[valid_train_flag]
+    train_dst_l = dst_l[valid_train_flag]
+    train_ts_l = ts_l[valid_train_flag]
+    train_e_idx_l = e_idx_l[valid_train_flag]
+    train_label_l = label_l[valid_train_flag]
+
+    # use the validation as test dataset
+    test_src_l = src_l[valid_val_flag]
+    test_dst_l = dst_l[valid_val_flag]
+    test_ts_l = ts_l[valid_val_flag]
+    test_e_idx_l = e_idx_l[valid_val_flag]
+    test_label_l = label_l[valid_val_flag]
+else:
+    logger.info('Training use all train data')
+    valid_train_flag = (ts_l <= test_time)  
+    train_src_l = src_l[valid_train_flag]
+    train_dst_l = dst_l[valid_train_flag]
+    train_ts_l = ts_l[valid_train_flag]
+    train_e_idx_l = e_idx_l[valid_train_flag]
+    train_label_l = label_l[valid_train_flag]
+
+    # use the true test dataset
+    test_src_l = src_l[valid_test_flag]
+    test_dst_l = dst_l[valid_test_flag]
+    test_ts_l = ts_l[valid_test_flag]
+    test_e_idx_l = e_idx_l[valid_test_flag]
+    test_label_l = label_l[valid_test_flag]
 
 # create two neighbor finders to handle graph extraction.
 # for transductive mode all phases use full_ngh_finder, for inductive node train/val phases use the partial one
@@ -100,9 +119,6 @@ for src, dst, eidx, ts in zip(src_l, dst_l, e_idx_l, ts_l):
 full_ngh_finder = NeighborFinder(full_adj_list, bias=args.bias, use_cache=NGH_CACHE, sample_method=args.pos_sample)
 partial_adj_list = [[] for _ in range(max_idx + 1)]
 for src, dst, eidx, ts in zip(train_src_l, train_dst_l, train_e_idx_l, train_ts_l):
-    partial_adj_list[src].append((dst, eidx, ts))
-    partial_adj_list[dst].append((src, eidx, ts))
-for src, dst, eidx, ts in zip(val_src_l, val_dst_l, val_e_idx_l, val_ts_l):
     partial_adj_list[src].append((dst, eidx, ts))
     partial_adj_list[dst].append((src, eidx, ts))
 partial_ngh_finder = NeighborFinder(partial_adj_list, bias=args.bias, use_cache=NGH_CACHE, sample_method=args.pos_sample)
@@ -134,18 +150,105 @@ criterion = torch.nn.BCELoss()
 early_stopper = EarlyStopMonitor(tolerance=TOLERANCE)
 
 # start train and val phases
-train_val(train_val_data, cawn, args.mode, BATCH_SIZE, NUM_EPOCH, criterion, optimizer, early_stopper, ngh_finders, rand_samplers, logger)
+#train_val(train_val_data, cawn, args.mode, BATCH_SIZE, NUM_EPOCH, criterion, optimizer, early_stopper, ngh_finders, rand_samplers, logger)
+
+
+if mode == 't':  # transductive
+    cawn.update_ngh_finder(full_ngh_finder)
+elif mode == 'i':  # inductive
+    cawn.update_ngh_finder(partial_ngh_finder)
+else:
+    raise ValueError('training mode {} not found.'.format(mode))
+
+num_instance = len(train_src_l)
+num_batch = math.ceil(num_instance / BATCH_SIZE)
+logger.debug('num of training instances: {}'.format(num_instance))
+logger.debug('num of batches per epoch: {}'.format(num_batch))
+idx_list = np.arange(num_instance)
+
+
+def eval_epoch(src_l, dst_l, ts_l, label_l, batch_size, model):
+    pred_prob = np.zeros(len(src_l))
+    loss = 0
+    num_instance = len(src_l)
+    num_batch = math.ceil(num_instance / batch_size)
+    with torch.no_grad():
+        model.eval()
+        for k in range(num_batch):          
+            s_idx = k * batch_size
+            e_idx = min(num_instance - 1, s_idx + batch_size)
+            src_l_cut = src_l[s_idx:e_idx]
+            dst_l_cut = dst_l[s_idx:e_idx]
+            ts_l_cut = ts_l[s_idx:e_idx]
+            label_l_cut = label_l[s_idx:e_idx]
+            size = len(src_l_cut)
+            lr_prob = model.contrast(src_l_cut, dst_l_cut, None, ts_l_cut, e_l_cut)
+            src_label = torch.from_numpy(label_l_cut).float().to(device)
+            loss += criterion(lr_prob, src_label).item()
+            pred_prob[s_idx:e_idx] = lr_prob.cpu().numpy()
+
+    auc_roc = roc_auc_score(label_l, pred_prob)
+    return auc_roc, loss / num_instance
+
+
+for epoch in tqdm(range(args.n_epoch)):
+    lr_pred_prob = np.zeros(len(train_src_l))
+    np.random.shuffle(idx_list)
+    logger.info('start {} epoch'.format(epoch))
+    acc = []
+    for k in range(num_batch):
+        # generate training mini-batch
+        s_idx = k * BATCH_SIZE
+        e_idx = min(num_instance - 1, s_idx + BATCH_SIZE)
+        if s_idx == e_idx:
+            continue
+        batch_idx = idx_list[s_idx:e_idx]
+        src_l_cut, dst_l_cut = train_src_l[batch_idx], train_dst_l[batch_idx]
+        ts_l_cut = train_ts_l[batch_idx]
+        e_l_cut = train_e_idx_l[batch_idx]
+        label_l_cut = train_label_l[batch_idx]  # currently useless since we are not predicting edge labels
+        
+        size = len(src_l_cut)
+        
+        optimizer.zero_grad()
+        cawn.train()
+        lr_prob = cawn.contrast(src_l_cut, dst_l_cut, None, ts_l_cut, e_l_cut)
+        
+        src_label = torch.from_numpy(label_l_cut).float().to(device)
+        lr_loss = criterion(lr_prob, src_label)
+        lr_loss.backward()
+        optimizer.step()
+        auc.append(roc_auc_score(label_l_cut, lr_prob.cpu().numpy()))
+
+    #train_auc, train_loss = eval_epoch(train_src_l, train_dst_l, train_ts_l, train_label_l, BATCH_SIZE, lr_model, tgan)
+    #test_auc, test_loss = eval_epoch(test_src_l, test_dst_l, test_ts_l, test_label_l, BATCH_SIZE, lr_model, tgan)
+    #torch.save(lr_model.state_dict(), './saved_models/edge_{}_wkiki_node_class.pth'.format(DATA))
+    #logger.info(f'train auc: {train_auc}, test auc: {test_auc}')
+    logger.info('train auc: {}'.format(np.mean(auc)))
+    val_loss, val_auc = eval_epoch(test_src_l, test_dst_l, test_ts_l, test_label_l, BATCH_SIZE, cawn)
+    # early stop check and checkpoint saving
+    if early_stopper.early_stop_check(val_auc):
+        logger.info('No improvment over {} epochs, stop training'.format(early_stopper.max_round))
+        logger.info(f'Loading the best model at epoch {early_stopper.best_epoch}')
+        best_checkpoint_path = cawn.get_checkpoint_path(early_stopper.best_epoch)
+        cawn.load_state_dict(torch.load(best_checkpoint_path))
+        logger.info(f'Loaded the best model at epoch {early_stopper.best_epoch} for inference')
+        cawn.eval()
+        break
+    else:
+        torch.save(cawn.state_dict(), cawn.get_checkpoint_path(epoch))
 
 # final testing
 cawn.update_ngh_finder(full_ngh_finder)  # remember that testing phase should always use the full neighbor finder
-test_acc, test_ap, test_f1, test_auc = eval_one_epoch('test for {} nodes'.format(args.mode), cawn, test_rand_sampler, test_src_l, test_dst_l, test_ts_l, test_label_l, test_e_idx_l)
-logger.info('Test statistics: {} all nodes -- acc: {}, auc: {}, ap: {}'.format(args.mode, test_acc, test_auc, test_ap))
-test_new_new_acc, test_new_new_ap, test_new_new_auc, test_new_old_acc, test_new_old_ap, test_new_old_auc = [-1]*6
-if args.mode == 'i':
-    test_new_new_acc, test_new_new_ap, test_new_new_f1, test_new_new_auc = eval_one_epoch('test for {} nodes'.format(args.mode), cawn, test_rand_sampler, test_src_new_new_l, test_dst_new_new_l, test_ts_new_new_l, test_label_new_new_l, test_e_idx_new_new_l)
-    logger.info('Test statistics: {} new-new nodes -- acc: {}, auc: {}, ap: {}'.format(args.mode, test_new_new_acc, test_new_new_auc,test_new_new_ap ))
-    test_new_old_acc, test_new_old_ap, test_new_old_f1, test_new_old_auc = eval_one_epoch('test for {} nodes'.format(args.mode), cawn, test_rand_sampler, test_src_new_old_l, test_dst_new_old_l, test_ts_new_old_l, test_label_new_old_l, test_e_idx_new_old_l)
-    logger.info('Test statistics: {} new-old nodes -- acc: {}, auc: {}, ap: {}'.format(args.mode, test_new_old_acc, test_new_old_auc, test_new_old_ap))
+#test_acc, test_ap, test_f1, test_auc = eval_one_epoch('test for {} nodes'.format(args.mode), cawn, test_rand_sampler, test_src_l, test_dst_l, test_ts_l, test_label_l, test_e_idx_l)
+test_loss, test_auc = eval_epoch(test_src_l, test_dst_l, test_ts_l, test_label_l, BATCH_SIZE, cawn)
+logger.info('Test statistics: {} all nodes -- auc: {}'.format(args.mode, test_auc))
+#test_new_new_acc, test_new_new_ap, test_new_new_auc, test_new_old_acc, test_new_old_ap, test_new_old_auc = [-1]*6
+#if args.mode == 'i':
+#    test_new_new_acc, test_new_new_ap, test_new_new_f1, test_new_new_auc = eval_one_epoch('test for {} nodes'.format(args.mode), cawn, test_rand_sampler, test_src_new_new_l, test_dst_new_new_l, test_ts_new_new_l, test_label_new_new_l, test_e_idx_new_new_l)
+#    logger.info('Test statistics: {} new-new nodes -- acc: {}, auc: {}, ap: {}'.format(args.mode, test_new_new_acc, test_new_new_auc,test_new_new_ap ))
+#    test_new_old_acc, test_new_old_ap, test_new_old_f1, test_new_old_auc = eval_one_epoch('test for {} nodes'.format(args.mode), cawn, test_rand_sampler, test_src_new_old_l, test_dst_new_old_l, test_ts_new_old_l, test_label_new_old_l, test_e_idx_new_old_l)
+#    logger.info('Test statistics: {} new-old nodes -- acc: {}, auc: {}, ap: {}'.format(args.mode, test_new_old_acc, test_new_old_auc, test_new_old_ap))
 
 # save model
 logger.info('Saving CAWN model ...')
@@ -153,7 +256,7 @@ torch.save(cawn.state_dict(), best_model_path)
 logger.info('CAWN model saved')
 
 # save one line result
-save_oneline_result('log/', args, [test_acc, test_auc, test_ap, test_new_new_acc, test_new_new_ap, test_new_new_auc, test_new_old_acc, test_new_old_ap, test_new_old_auc])
+save_oneline_result('log/', args, [test_auc])
 # save walk_encodings_scores
 # checkpoint_dir = '/'.join(cawn.get_checkpoint_path(0).split('/')[:-1])
 # cawn.save_walk_encodings_scores(checkpoint_dir)
