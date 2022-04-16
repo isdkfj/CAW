2181b047581af38a4ef27e02474cd059e71c35b2
叶芃 <isdkfj@qq.com>
2022-04-16 12:36:35 +0800

    Update log2.py

--- log2.py
+++ log2.py
@@ -17,7 +17,7 @@ def set_up_logger(args, sys_argv):
     fh = logging.FileHandler(file_path)
     fh.setLevel(logging.DEBUG)
     ch = logging.StreamHandler()
-    ch.setLevel(logging.WARN)
+    ch.setLevel(logging.DEBUG)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     fh.setFormatter(formatter)
     ch.setFormatter(formatter)
